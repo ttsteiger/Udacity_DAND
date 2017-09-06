be6f544ec28178c4b019ef7d4230b99e7ba85a05
titian <titian.steiger@gmail.com>
2017-09-06 21:28:37 +0200

    Finished p5.

--- DAND_p5/helper_functions.py
+++ DAND_p5/helper_functions.py
@@ -126,6 +126,8 @@ def get_classifier_scores(clf, X, y, random_state=None):
     Returns:
     	List containing the mean accuracy, precision and recall for the 
     	evaluated classifier.
+
+    	[accuracy, precision, recall]
     """
     
     # check if data set is in a dataframe, if so convert it to a numpy array
@@ -168,6 +170,10 @@ def get_multi_classifier_scores(names, classifiers, X, y, random_state=None):
 	Returns:
 		Dictionary containing dictionairies for each classifier name key with
     	accuracy, precision and recall key-value pairs.
+
+    	{clf_name1: {accuracy: ..., precision: ..., recall: ...},
+    	 clf_name2: {...}, 
+    	 ...}
     """
     
     clf_scores = OrderedDict()
@@ -203,6 +209,10 @@ def find_best_parameters(names, classifiers, X, y, param_grid, score='accuracy',
     Returns:
     	Dictionary containing dictionairies for each classifier name key with
     	the optimally performing parameter set and the corresponding score.
+
+    	{clf_name1: {parameters: {p1: v1, p2: v2, ...}, score: ...},
+    	 clf_name2: {...}, 
+    	 ...}
     """
     
     # check if data set is in a dataframe, if so convert it to a numpy array
@@ -251,9 +261,9 @@ def optimize_features_and_parameters(names, classifiers, X, y, features,
     	the optimally performing parameter set, the optimum number of top 
     	features to use and the corresponding score.
 
-    	{clf_name1: {prop1: ..., prop2: ..., ...},
-    		 clf_name2: {...}, 
-    		 ...}
+    	{clf_name1: {parameters: {p1: v1, p2: v2, ...}, n: ..., score: ...},
+    	 clf_name2: {...}, 
+    	 ...}
     """
 
     # perform parameter optimization for varying number of input features
@@ -289,13 +299,14 @@ def optimize_features_and_parameters(names, classifiers, X, y, features,
 
 def print_classifier_table(scores):
     """
-	Print out a table containing ...
+	Print out a table displaying scores, number of features or parameter 
+	settings for each classifier in the scores dictionary in a separate row.
 
     Args:
     	scores: Dictionary containing informmation about classifiers that should
     		be displayed in the table.
     		
-    		{clf_name1: {prop1: ..., prop2: ..., ...},
+    		{clf_name1: {prop1: v1, prop2: v2, ...},
     		 clf_name2: {...}, 
     		 ...}
     """
@@ -348,7 +359,14 @@ def print_classifier_table(scores):
 
 def scatter_plot(df, x, y, normalize=True):
     """
-    
+    Create a scatter plot of column x vs. columns y. Also normalizes the two
+    variables using the MinMaxScaler() from scikit-learn.
+
+    Args:
+    	df: DataFrame containing the feature columns.
+    	x: Header string of the x column. 
+    	y: Header string of the y column.
+    	normalize: Enable or disable feature normalization.
     """
     poi_df = df[df['poi'] == True]
     x_poi = poi_df[x].fillna(value=0).values.reshape(-1, 1)
@@ -379,18 +397,17 @@ def generate_meshgrid(x, y, h=.01):
     """
     Source: http://scikit-learn.org/stable/auto_examples/svm/plot_iris.html
     
-    Create a mesh of points to plot in.
+    Create a meshgrid of points to plot in.
     
-    Parameters
-    ----------
-    x: data to base x-axis meshgrid on
-    y: data to base y-axis meshgrid on
-    h: stepsize for meshgrid, optional
+    Args:
+    	x: 1D numpy array to base x-axis meshgrid on.
+    	y: 1D numpy array to base y-axis meshgrid on.
+    	h: Stepsize for meshgrid.
 
-    Returns
-    -------
-    xx, yy : ndarray
+    Returns:
+    	xx, yy: 1D arrays containing x- and y-axis meshgrids.
     """
+
     x_min, x_max = x.min() - 0.1, x.max() + 0.1
     y_min, y_max = y.min() - 0.1, y.max() + 0.1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
@@ -404,15 +421,16 @@ def plot_boundaries(ax, clf, xx, yy, **params):
     
     Plot the decision boundaries for a classifier.
 
-    Parameters
-    ----------
-    ax: matplotlib axes object
-    clf: a classifier
-    xx: meshgrid ndarray
-    yy: meshgrid ndarray
-    params: dictionary of params to pass to contourf, optional
+    Args:
+    	ax: Matplotlib axes object.
+    	clf: Scikit-learn classifier object.
+    	xx: 1D array containing x-axis meshgrid.
+    	yy: 1D array containing y-axis meshgrid.
+    	params: Dictionary of parameters to pass to contourf function.
     """
+    
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     out = ax.contourf(xx, yy, Z, **params)
+    
     return out
